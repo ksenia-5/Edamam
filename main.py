930108d3dadf16d93297ae1b8d79eecdf7c34903
ksenia-5 <xenia.germanovich@gmail.com>
2023-03-29 20:14:51 +0100

    Pseudocode for main app, get recipe ingredients/ label

---
+++ main.py
@@ -0,0 +1,27 @@
+## Welcome message
+
+## Prompt user to input ingredients (one or more) separated by comma
+
+## Parse the input to list,
+## store as INGREDIENTS = []
+
+## recipes = []
+## Get recipes query, add response results to recipe list
+## query until response is empty
+
+## convert recipes to pandas dataframe, for easier sorting
+## store id, health label, cooking time, number of ingredients, dish type
+
+## user imputs sort parameter
+
+## sort by parameter
+
+## present top three, numbered 1,2,3
+
+## user chooses recipe to print
+## by inputing value
+
+## make a request to api for chosen recipe (id)
+## label, cooking time, ingredients, instructions and image
+
+## write response to file and save 
--- start.py
+++ start.py
@@ -4,28 +4,40 @@ Register to get an APP ID and key https://developer.edamam.com/
 import os
 import sys 
 import requests
+import json
 
 cdir = os.getcwd() # current directory
 pdir = os.path.dirname(cdir) # parent directory
-sys.path.append(os.path.join(pdir,"creds.py")) #add path of parent direcotry to system
-from creds import YOUR_APP_ID, YOUR_APP_KEY # import private ID and KEY stored in creds.py file in parent directory
+fpath = os.path.join(pdir,"edamam_creds.json") #add path of parent direcotry to system
+
+# convert creds to json file?
+with open(fpath) as user_file:
+    creds = json.load(user_file)
+
+YOUR_APP_ID = creds['YOUR_APP_ID']
+YOUR_APP_KEY = creds['YOUR_APP_KEY']
 
 # alternatively imput ID and KEY value on each run
-#YOUR_APP_ID = input("Input your application id: ")
+# YOUR_APP_ID = input("Input your application id: ")
 # YOUR_APP_KEY = input("Input your application id: ")
 
 # INGREDIENT = input("Input ingredient: ")
-INGREDIENT = "tofu"
+# INGREDIENT = ["tofu","chicken","tomato"]
 
-url  = f"https://api.edamam.com/search?q={INGREDIENT}&app_id={YOUR_APP_ID}&app_key={YOUR_APP_KEY}"
 
-response = requests.get(url)
-response = response.json()
-# print(response)
 
-recipes = response['hits']
+def get_recipe_labels(ingredient):
+    url  = f"https://api.edamam.com/search?q=['tofu','chicken','tomato']&from=0&to=10&app_id={YOUR_APP_ID}&app_key={YOUR_APP_KEY}"
+    # url  = f"https://api.edamam.com/search?q={INGREDIENT}&from=0&to=10&app_id={YOUR_APP_ID}&app_key={YOUR_APP_KEY}"
+    # url1 = f"https://api.edamam.com/api/v2?q={INGREDIENT}&app_id={YOUR_APP_ID}&app_key={YOUR_APP_KEY}"
+    response = requests.get(url)
+    response = response.json()
+    recipes = response['hits']
+    # list comprehension
+    recipe_ingredients = [[recipes[i]['recipe']["ingredientLines"] for i in range(len(recipes))]] 
+    recipe_labels = [recipes[i]['recipe']['label'] for i in range(len(recipes))]
+    return recipe_ingredients
 
-for i in range(len(recipes)):
-    recipe = recipes[i]['recipe']
-    recipe_title = recipe['label']
-    print(recipe_title)
+recipes = get_recipe_labels("tofu")
+for recipe in recipes:
+    print(recipe)
